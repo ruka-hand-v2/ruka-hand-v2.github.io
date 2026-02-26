dccbc8db8739d4bff6b7abd8210cd5486c7db987
pianapolataa <sissi.hu@hotmail.com>
2026-02-26 17:28:18 -0500

    add videos

--- static/js/index.js
+++ static/js/index.js
@@ -49,3 +49,20 @@ document.addEventListener('DOMContentLoaded', function () {
 	});
   });
   
+  document.addEventListener('DOMContentLoaded', function () {
+	const tabs = document.querySelectorAll('#ftf-video-tabs-policy li');
+	const contents = document.querySelectorAll('#ftf-video-display-policy .video-content');
+  
+	tabs.forEach(tab => {
+	  tab.addEventListener('click', () => {
+		const targetId = tab.getAttribute('data-target');
+  
+		tabs.forEach(t => t.classList.remove('is-active'));
+  
+		contents.forEach(content => content.style.display = 'none');
+  
+		tab.classList.add('is-active');
+		document.getElementById(targetId).style.display = '';
+	  });
+	});
+  });
